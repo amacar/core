b816b446dbdde58345b60c8474a6b716154f3f7d
Amadej Pevec <amadej.pevec@gmail.com>
2020-04-26 01:47:44 +0200

    Add home sensor data plugin

--- packages/core/bin/config/testnet/app.js
+++ packages/core/bin/config/testnet/app.js
@@ -3,35 +3,35 @@ module.exports = {
         core: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: ["@arkecosystem/core-magistrate-transactions", "@amacar/home-sensor-data"],
                 },
             },
         },
         relay: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: ["@arkecosystem/core-magistrate-transactions", "@amacar/home-sensor-data"],
                 },
             },
         },
         forger: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: ["@arkecosystem/core-magistrate-transactions", "@amacar/home-sensor-data"],
                 },
             },
         },
         chain: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: ["@arkecosystem/core-magistrate-transactions", "@amacar/home-sensor-data"],
                 },
             },
         },
         snapshot: {
             run: {
                 plugins: {
-                    include: ["@arkecosystem/core-magistrate-transactions"],
+                    include: ["@arkecosystem/core-magistrate-transactions", "@amacar/home-sensor-data"],
                 },
             },
         },
